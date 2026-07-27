7a18401b151b8e4f255d32542744e0cf487bb57b
dev2 <gerardotataje.dev2@hotmail.com>
2026-07-27 00:53:27 -0500

    feature: GetByName

--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -26,6 +26,14 @@ public class CategoryRepository:ICategoryRepository
                              .FirstOrDefaultAsync(c => c.Id == id);
     }
 
+     public async Task<Category?> GetByNameAsync(string name)
+    {
+        // En Cosmos, WithPartitionKey optimiza la búsqueda
+        return await _context.Categories
+                             
+                             .FirstOrDefaultAsync(c => c.Name == name);
+    }
+
     public async Task<Category> AddAsync(Category category)
     {
         _context.Categories.Add(category);
--- Repositories/ICategoryRepository.cs
+++ Repositories/ICategoryRepository.cs
@@ -4,6 +4,7 @@ public interface ICategoryRepository
 {
     Task<IEnumerable<Category>> GetAllAsync();
     Task<Category?> GetByIdAsync(string id);
+     Task<Category?> GetByNameAsync(string name);
     Task<Category> AddAsync(Category category);
     Task<Category?> UpdateAsync(string id, Category category);
     Task<bool> DeleteAsync(string id);
